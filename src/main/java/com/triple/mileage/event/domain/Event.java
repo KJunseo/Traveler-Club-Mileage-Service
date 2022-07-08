2416ff618ec573007a1a2276a7e10499cc7df7df
Air Junseo <air.junseo@gmail.com>
2022-07-09 01:18:49 +0900

    feat: 초기 도메인 구조 설계 (#17)

---
+++ src/main/java/com/triple/mileage/event/domain/Event.java
@@ -0,0 +1,27 @@
+package com.triple.mileage.event.domain;
+
+import java.util.UUID;
+import javax.persistence.*;
+
+import com.triple.mileage.review.domain.Review;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    private EventType type;
+
+    @Enumerated(EnumType.STRING)
+    private EventAction action;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Review review;
+}
